a93164b10e85325bda9a38460bb71de918b788d1
EdwardAsher <32741636+EdwardAsher@users.noreply.github.com>
2018-03-14 14:56:57 -0500

    Fixed the movement animation
    Looped the animations correctly

--- QWOPBO/Assets/Scripts/Move.cs
+++ QWOPBO/Assets/Scripts/Move.cs
@@ -6,19 +6,26 @@ public class Move : MonoBehaviour {
 
     private string moveInputAxis = "Vertical";
 
+    public Animator anim;
 
     public float moveSpeed = 2;
 
     #region MonoBehaviour API
-
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         float moveAxis = Input.GetAxis(moveInputAxis);
 
-
         ApplyInput(moveAxis);
+        if (Input.GetKeyDown("i"))
+        {
+            anim.CrossFade("Punch", 0.1f);
+        }
     }
 
     private void ApplyInput(float moveInput)
