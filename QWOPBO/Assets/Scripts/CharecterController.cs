f604acc3a3bc369e897a31d8f033bc6cd7176f16
mynameisabdul <amalik11@uic.edu>
2018-03-18 01:15:31 -0500

    WASD player movement
    Changed controlls to WASD and made prefabs for two characters with walking and running. Made a new scene to test it on. Presss and hold left click and W to run.

---
+++ QWOPBO/Assets/Scripts/CharecterController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharecterController : MonoBehaviour {
+
+    public Animator a;
+    public Rigidbody r;
+    public float sprint;
+
+    private float H;
+    private float V;
+    private float speedBoost;
+
+    public float rotationRate = 60;
+    public float moveSpeed = 5;
+
+
+    // Use this for initialization
+    void Start () {
+        a = GetComponent<Animator>();
+        r = GetComponent<Rigidbody>();
+        speedBoost = 1.0f;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        H = Input.GetAxis("Horizontal");
+        V = Input.GetAxis("Vertical");
+        Running();
+
+        Turn(H);
+
+
+        if (Input.GetKeyUp(KeyCode.W) && Input.GetKeyUp(KeyCode.S))
+        {
+            r.Sleep();
+            r.velocity = Vector3.zero;
+            r.angularVelocity = Vector3.zero;
+            
+        }
+        else
+        {
+            Move(V);
+        }
+
+        /*
+
+        float movex = H * 20f * Time.deltaTime;
+        float movez = V * 50f * Time.deltaTime;
+
+        r.velocity = new Vector3(movex, 0f, movez);
+        */
+    }
+
+    void FixedUpdate()
+    {
+        a.SetFloat("walk", V);
+        //a.SetFloat("turn", H);
+        a.SetFloat("run", sprint);
+
+    }
+
+    void Running()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            sprint = 1.0f;
+            speedBoost = 1.5f;
+
+        }
+        else
+        {
+            sprint = 0.0f;
+            speedBoost = 1.0f;
+        }
+    }
+
+    private void Move(float input)
+    {
+        r.AddForce(transform.forward * (input * speedBoost) * moveSpeed);
+    }
+
+    private void Turn(float input)
+    {
+        transform.Rotate(0, input * rotationRate * Time.deltaTime, 0);
+    }
+}
