4818824fc0df2775a442f7986440ee3417d6a73e
mynameisabdul <amalik11@uic.edu>
2018-04-18 17:21:44 -0500

    got rid of some stuff

--- QWOPBO/Assets/Scripts/FSM/FSMAction.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class FSMAction : ScriptableObject 
-{
-    public abstract void Act (FSMStateController controller);
-}
