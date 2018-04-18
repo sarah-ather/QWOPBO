f573222ea730dedcfa15f5120cc9b6d886637a55
Edward Reyes <32741636+EdwardAsher@users.noreply.github.com>
2018-04-18 11:24:31 -0500

    Changed the lock trap
    Changed the behavior of the lock trap to change to a green open version when the key is gotten, also fixed some z fighting in the main map.

--- QWOPBO/Assets/Scripts/RemoveWall.cs
+++ QWOPBO/Assets/Scripts/RemoveWall.cs
@@ -4,7 +4,21 @@ using UnityEngine;
 
 public class RemoveWall : MonoBehaviour {
     public Object Barrier;
+    public GameObject lockTrap;
+    public GameObject lockTrapUnlocked;
     private bool flag = false;
+    private bool lockChange = false;
+
+    private void Update()
+    {
+        if (flag == true && lockChange == false)
+        {
+            if (lockTrapUnlocked!=null)
+            Instantiate(lockTrapUnlocked, lockTrap.transform.position, lockTrap.transform.rotation);
+            Destroy(lockTrap);
+            lockChange = true;
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
